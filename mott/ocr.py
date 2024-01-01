583d491e1a9c37a9da1e65cb61ddc84a9b8a4c47
Toby Searle <14909402+twsearle@users.noreply.github.com>
2024-01-01 12:01:43 +0000

    Use asyncio for retrieving and processing images (#3)
    * add further ocr tests
    * use asyncio for retrieving and processing images

--- mott/ocr.py
+++ mott/ocr.py
@@ -3,13 +3,42 @@ import logging
 from PIL import Image, ImageOps
 import pytesseract
 import validators
-import requests
+import aiohttp
 import io
 
 logger_discord = logging.getLogger("discord")
 
 from mott.exceptions import MottException
 from urllib.parse import urlparse
+
+
+class OCRInvalidURLError(MottException):
+    def __init__(self, URI, message=""):
+        if message == "":
+            self.message = f"Invalid image URL: {URI}"
+        else:
+            self.message = message
+        super().__init__(self.message)
+
+
+class OCRaUECNotFoundError(MottException):
+    def __init__(self, contents, message=""):
+        if message == "":
+            self.message = f"OCR: aUEC not found in image contents"
+        else:
+            self.message = message
+        logger_discord.debug(f"Failed to detect aUEC in image contents: '{contents}'")
+        super().__init__(self.message)
+
+
+class OCRNumberNotFoundError(MottException):
+    def __init__(self, number_string, message=""):
+        if message == "":
+            self.message = f"OCR: number not found in image contents"
+        else:
+            self.message = message
+        logger_discord.debug(f"Failed to detect a number of aUEC in: '{number_string}'")
+        super().__init__(self.message)
 
 
 def uri_validator(x):
@@ -30,20 +59,30 @@ def convert_image_format(image, output_format=None):
 
 
 class OCR:
-    def __init__(self, URI):
+    @classmethod
+    async def create(cls, URI):
+        self = cls()
         self.uri = URI
-        if uri_validator(URI):
-            validation = validators.url(URI)
+        if uri_validator(self.uri):
+            validation = True
+            try:
+                validation = validators.url(self.uri)
+            except validators.utils.ValidationError:
+                raise OCRInvalidURLError(self.uri)
             if not validation:
-                raise MottException(f"Invalid image URL: {URI}")
-            r = requests.get(URI, stream=True)
-            if r.status_code != 200:
-                raise MottException(
-                    f"Failed to read: {URI} requests status_code: {r.status_code}"
-                )
-            self.image = Image.open(io.BytesIO(r.content))
+                raise OCRInvalidURLError(self.uri)
+
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.uri) as r:
+                    if r.status != 200:
+                        raise MottException(
+                            f"Failed to read: {self.uri} status_code: {r.status_code}"
+                        )
+                    req_contents = await r.read()
+                    self.image = Image.open(io.BytesIO(req_contents))
         else:
-            self.image = Image.open(URI)
+            self.image = Image.open(self.uri)
+        return self
 
     def contains_auec(self, contents) -> int:
         auec_variants = []
@@ -58,10 +97,7 @@ class OCR:
             if number_end >= 0:
                 break
         if number_end < 0:
-            logger_discord.debug(
-                f"Failed to detect aUEC in image contents: '{contents}'"
-            )
-            raise MottException(f"OCR failure: aUEC not found in image contents")
+            raise OCRaUECNotFoundError(contents)
         return number_end
 
     def auec_value(self, contents):
@@ -76,17 +112,14 @@ class OCR:
         try:
             value = int(number_string[::-1])
         except ValueError:
-            logger_discord.debug(
-                f"Failed to detect a number of aUEC in image contents: '{contents}'"
-            )
-            raise MottException(f"OCR failure: number not found in image contents")
+            raise OCRNumberNotFoundError(contents)
         return value
 
-    def image_to_auec(self) -> float:
+    async def image_to_auec(self) -> float:
         logger_discord.info(f' processing image URI: "{self.uri}"')
         self.image = convert_image_format(self.image, output_format="PNG")
         self.image = self.image.convert("RGB")
         self.image = ImageOps.invert(self.image)
         self.image = ImageOps.autocontrast(self.image, cutoff=(0, 95))
-        contents = pytesseract.image_to_string(self.image)
+        contents = pytesseract.image_to_string(self.image, config=r"--psm 4")
         return self.auec_value(contents)
--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -1,26 +1,80 @@
-from mott.exceptions import MottException
-from mott.ocr import OCR
-import pytest
-
 import logging
 from logging import StreamHandler
 
 logger = logging.getLogger("discord")
 logger.setLevel(logging.DEBUG)
-logger.addHandler(StreamHandler())
+
+from mott.exceptions import MottException
+from mott.ocr import (
+    OCR,
+    OCRInvalidURLError,
+    OCRaUECNotFoundError,
+    OCRNumberNotFoundError,
+    uri_validator,
+)
+import pytest
+import pytest_asyncio
 
 
-def test_ocr_local():
-    input_uri = "tests/data/bigmotradertest.jpeg"
-    auec = OCR(input_uri).image_to_auec()
-    assert auec == 820000
+@pytest_asyncio.fixture
+async def test_ocr():
+    return await OCR.create("tests/data/bigmotradertest.jpeg")
 
 
-def test_ocr_url():
-    url = "https://en.wikipedia.org/wiki/Test_card#/media/File:SMPTE_Color_Bars.svg"
-    with pytest.raises(MottException):
-        auec = OCR(url).image_to_auec()
-    url = "https://github.com/twsearle/mottbot/blob/main/tests/data/bigmotradertest.jpeg?raw=true"
+class TestOCR:
+    @pytest.mark.asyncio
+    async def test_ocr_local(self, test_ocr):
+        auec = await test_ocr.image_to_auec()
+        assert auec == 820000
 
-    auec = OCR(url).image_to_auec()
-    assert auec == 820000
+    @pytest.mark.asyncio
+    async def test_ocr_url(self):
+        url = "https://en.wikipedia.org/wiki/Test_card#/media/File:SMPTE_Color_Bars.svg"
+        assert uri_validator(url)
+        with pytest.raises(OCRInvalidURLError):
+            _ = await OCR.create(url)
+
+        url = "https://robertsspaceindustries.com/media/fupjr98kisd1fr/store_slideshow_large/BatCave_4k_v02.jpg"
+        with pytest.raises(OCRaUECNotFoundError):
+            ocr_obj = await OCR.create(url)
+            auec = await ocr_obj.image_to_auec()
+
+        url = "https://github.com/twsearle/mottbot/blob/main/tests/data/bigmotradertest.jpeg?raw=true"
+        ocr_obj = await OCR.create(url)
+        auec = await ocr_obj.image_to_auec()
+        assert auec == 820000
+
+    @pytest.mark.asyncio
+    async def test_contains_auec(self, test_ocr):
+        contents_throw = [" pounds", " notauec", "654jauec"]
+        for c in contents_throw:
+            with pytest.raises(OCRaUECNotFoundError):
+                test_ocr.contains_auec(c)
+
+        contents_and_number_end = {
+            "9,999 aUEC": 5,
+            "9,999  aUEC": 6,
+            "9,999 aVEC": 5,
+            "9,999 auec": 5,
+            "9,999 avec": 5,
+        }
+        for k, v in contents_and_number_end.items():
+            assert test_ocr.contains_auec(k) == v
+
+    @pytest.mark.asyncio
+    async def test_auec_value(self, test_ocr):
+        contents_throw = ["  aUEC", "abv avec"]
+        for c in contents_throw:
+            with pytest.raises(OCRNumberNotFoundError):
+                test_ocr.auec_value(c)
+
+        contents_and_value = {
+            "999 aUEC": 999,
+            "9,999 aUEC": 9999,
+            "junk9,999,999 aUECjunk": 9999999,
+            " 1,999,999 aUEC": 1999999,
+            "\t9,199,999 aUEC": 9199999,
+            "\n9,919,999 aUEC": 9919999,
+        }
+        for k, v in contents_and_value.items():
+            assert test_ocr.auec_value(k) == v
